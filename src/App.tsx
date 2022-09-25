394f365f0834102ec64896337c3687742c2fcd94
jackra1n <lok7ok@gmail.com>
2022-09-25 22:19:32 +0200

    change back to tsx (might not work)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,85 @@
-import React, { useState } from 'react';
+import React from "react";
 import "./App.css";
 
-var navApp = navigator.userAgent.toLowerCase();
+const digitecFetchUrl = "https://cors-anywhere.herokuapp.com/https://www.digitec.ch/api/graphql/get-social-shoppings";
 
-function HomePageGreeting() {
+const headersList = {
+  "Accept": "*/*",
+  "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+  "Content-Type": "application/json",
+  "Origin": "https://www.digitec.ch"
+}
 
-  const [isRedColor, setRedColor] = useState(false);
-  const toggleColor = () => setRedColor((prev) => !prev);
+const gqlBody = {
+  query: `query GET_SOCIAL_SHOPPINGS($take: Int, $latest: String) {
+    socialShopping(take: $take, latest: $latest) {
+      latestTransactionTimeStamp
+      items {
+        id
+        userName
+        cityName
+        dateTime
+        imageUrl
+        brandName
+        fullProductName
+        salesPrice {
+          amountIncl
+          amountExcl
+          currency
+          __typename
+        }
+        oAuthProviderName
+        targetUserName
+        quote
+        voteTypeId
+        productTypeName
+        socialShoppingTransactionTypeId
+        url
+        rating
+        searchString
+        __typename
+      }
+      __typename
+    }
+  }`,
+  variables: {"take":10,"latest":null}
+}
+const bodyContent =  JSON.stringify(gqlBody);
 
-  let osName = "Unknown OS";
-  if (navApp.indexOf("mac") != -1) {
-    osName = "Welcome to my Mac";
-  } else if (navApp.indexOf("win") != -1) {
-    osName = "Welcome to my Windows";
-  } else {
-    osName = "Welcome to my PC";
+export default class HomePageGreeting extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: [],
+      DataIsLoaded: false
+    };
   }
-
-  return (
-    <div className="App">
+  
+  componentDidMount() {
+    console.log("fetching data");
+    fetch(digitecFetchUrl, { 
+      method: "POST",
+      body: bodyContent,
+      headers: headersList
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        products: data.data.socialShopping.items,
+        DataIsLoaded: true
+      });
+    })
+  }
+  
+  render() {
+    const { DataIsLoaded, products } = this.state;
+    if (!DataIsLoaded) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
@@ -27,20 +88,13 @@ function HomePageGreeting() {
       <h1>Vite + React</h1>
       <div className="card">
         <p>
-          {osName}
+          {products.map((item) => (
+            <div>
+              <p><b>{item.userName}</b> bought <b>{item.fullProductName}</b> from <b>{item.brandName}</b></p>
+            </div>
+          ))}
         </p>
       </div>
-      <button
-        onClick={toggleColor}
-        style={{
-          background: isRedColor ? "red" : "black",
-          color: 'white'
-        }}
-      >
-        Button is {isRedColor ? "red" : "not red"}
-      </button>
     </div>
-  )
+  )};
 }
-
-export default HomePageGreeting
